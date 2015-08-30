6d5641f1b93e57d140547c4fd821b6f5052311c0
Jan Novak <novanojj@seznam.cz>
2015-08-30 22:36:32 +0200

    plain project

---
+++ MeteoController/src/meteocontroller/MeteoController.java
@@ -0,0 +1,16 @@
+package meteocontroller;
+
+/**
+ *
+ * @author Johnny
+ */
+public class MeteoController {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
